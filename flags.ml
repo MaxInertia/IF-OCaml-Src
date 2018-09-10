efdb46bd5e361012276d4c0e7811ce1ba6df8f29
Dorian Thiessen (dbt908) <dorian.thiessen@usask.ca>
2018-09-10 12:38:36 -0500

    Use continuation passing style to make parsing more readable
    - Compiler is complaining about line 60 and 63 when using string as the key. Does Map(X) use X as the key AND the value? Can we set a different type for both...?

--- flags.ml
+++ flags.ml
@@ -11,7 +11,7 @@ end;; open Flag;; *)
 
 module Flag : sig
     type t =
-        | UniFlag of string 
+        | UniFlag of string
         | ArgFlag of string * string
     val compare : t -> t -> int
 end = struct
@@ -35,33 +35,39 @@ open Flag;;
 module FlagMap =
     Map.Make(Flag);;
 
-(* FlagsMap.(empty |> add "A" "B");; *)
+let empty_flag_map = FlagMap.empty;;
 
-(* Try continuation passing here? *)
-let rec parse_flags ?(index=0) ?(acc=FlagMap.empty) args =
-    let candidate = args.(index) in
-    if Array.length args > (index + 1) then
-        if candidate.[0] = '-' then
-            if args.(index + 1).[0] = '-' then
-                let flag = UniFlag candidate in
-                parse_flags
-                    ~index:(index + 1)
-                    ~acc:(FlagMap.add flag acc)
-                    args
-            else
-                let flag = ArgFlag (candidate, args.(index+1)) in
-                parse_flags
-                    ~index:(index + 1)
-                    ~acc:(FlagMap.add flag acc)
-                    args
-        else parse_flags
-                ~index:(index + 1)
-                ~acc:acc
-                args
-    else acc;;
+let uni_flag = UniFlag "-a";;
+let uni_flag2 = UniFlag Sys.argv.(0);;
+let arg_flag = ArgFlag ("-a", "true");;
+
+let map_with_stuff = FlagMap.add uni_flag uni_flag empty_flag_map;;
+
+let rec start_parsing args =
+    parse_fs 0 FlagMap.empty args;
+and parse_fs i acc args =
+    let candidate = args.(i) in
+    if Array.length args > (i + 1)
+    then parse_current_arg i acc args
+    else acc;
+and parse_current_arg i acc args =
+    if args.(i).[0] = '-'
+    then parse_flag i acc args
+    else parse_fs (i + 1) acc args;
+and parse_flag i acc args =
+    if args.(i + 1).[0] = '-'
+    then parse_fs (i + 1) (
+            FlagMap.add (*args.(i)*) uni_flag (ArgFlag (args.(i), args.(i + 1))) acc
+         ) args
+    else parse_fs (i + 1) (
+            FlagMap.add (*args.(i)*) uni_flag (UniFlag args.(i)) acc
+         ) args;;
 
 let xs = [| "a"; "b"; "-f"; "true"; "apples"; "-c"; "camera!"; |];;
 
+start_parsing xs;;
+
+(*
 let rec print_flags flags =
     FlagMap.iter (
         fun k v -> printf "(%s:\t%s)\n" k v
@@ -74,3 +80,4 @@ then
 else
     print_endline "flag\tvalue";
     print_flags fas;;
+*)
